2cd487e6e6c3b9d6f810e59f1eeaaa6c7cd8672b
jacklenjl <kunalbaghelkb@gmail.com>
2019-08-22 09:07:34 +0530

    auth fix

--- web-app/src/auth/auth.controller.ts
+++ web-app/src/auth/auth.controller.ts
@@ -1,4 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { Controller,Get } from '@nestjs/common';
+import { AuthService } from './auth.service';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+    constructor(
+        private readonly authService: AuthService
+    ){}
+
+@Get('test')
+checkhello()
+{
+return  this.authService.checkhellomain();   
+}
+
+}
--- web-app/src/auth/auth.service.ts
+++ web-app/src/auth/auth.service.ts
@@ -1,4 +1,12 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+
+
+    checkhellomain()
+    {
+        return "this is check hello";
+    }
+
+}
